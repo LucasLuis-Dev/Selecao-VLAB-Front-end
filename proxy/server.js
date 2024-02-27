2f8885cf3238cbedd22532c84456717c4622378f
LucasLuis-Dev <lucasluisouza@gmail.com>
2024-02-27 15:22:23 -0300

    feat: addition of axios and /games route for API requests in server.js

--- proxy/server.js
+++ proxy/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = 4123;
@@ -14,6 +15,16 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/games', async (req, res) => {
+    try {
+      const response = await axios.get('https://www.freetogame.com/api/games');
+      res.json(response.data);
+    } catch (error) {
+      console.error('Erro ao obter os jogos:', error);
+      res.status(500).json({ error: 'Erro ao obter os jogos' });
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
